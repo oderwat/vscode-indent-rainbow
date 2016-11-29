c9dcd78b40426c1816e234def05fa72ff2e3d88b
Gavin Dinubilo <gavincodes@gmail.com>
2016-11-29 13:41:53 -0800

    - Adds an error_color config option - Makes it so colors config option can be any size you'd like - Minor code optimizations - Makes code comply with TSLint

--- extension.ts
+++ extension.ts
@@ -5,30 +5,39 @@ import * as vscode from 'vscode';
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
 
-  // create a decorator types that we use to decorate indent levels
-  var decorationType = [];
+  // Create a decorator types that we use to decorate indent levels
+  let decorationTypes = [];
 
-  var doIt = false;
-  var clearMe = false;
-  var currentLanguageId = null;
+  let doIt = false;
+  let clearMe = false;
+  let currentLanguageId = null;
 
-  var activeEditor = vscode.window.activeTextEditor;
+  let activeEditor = vscode.window.activeTextEditor;
 
-  var colors = vscode.workspace.getConfiguration('indentRainbow')['colors'] || [
+  // Error color gets shown when tabs aren't right, 
+  //  e.g. when you have your tabs set to 2 spaces but the indent is 3 spaces 
+  const error_color = vscode.workspace.getConfiguration('indentRainbow')['error_color'] || "rgba(128,32,32,0.3)";
+  let error_decoration_type = vscode.window.createTextEditorDecorationType({
+      backgroundColor: error_color 
+  });
+
+  // Colors will cycle through, and can be any size that you want
+  const colors = vscode.workspace.getConfiguration('indentRainbow')['colors'] || [
     "rgba(64,64,16,0.3)",
     "rgba(32,64,32,0.3)",
     "rgba(64,32,64,0.3)",
-    "rgba(16,48,48,0.3)",
-    "rgba(128,32,32,0.3)"
+    "rgba(16,48,48,0.3)"
   ];
 
-  for(var i=0; i<colors.length; i++) {
-    decorationType[i] = vscode.window.createTextEditorDecorationType({
-        backgroundColor: colors[i] })
-  }
+  // Loops through colors and creates decoration types for each one
+  colors.forEach((color, index) => {
+    decorationTypes[index] = vscode.window.createTextEditorDecorationType({
+      backgroundColor: color 
+    });
+  });
 
   if(activeEditor) {
-    indentConfig()
+    indentConfig();
   }
 
   if (activeEditor && checkLanguage()) {
@@ -38,7 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.onDidChangeActiveTextEditor(editor => {
     activeEditor = editor;
     if (editor) {
-      indentConfig()
+      indentConfig();
     }
 
     if (editor && checkLanguage()) {
@@ -48,7 +57,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   vscode.workspace.onDidChangeTextDocument(event => {
     if(activeEditor) {
-      indentConfig()
+      indentConfig();
     }
 
     if (activeEditor && event.document === activeEditor.document && checkLanguage()) {
@@ -57,7 +66,7 @@ export function activate(context: vscode.ExtensionContext) {
   }, null, context.subscriptions);
 
   function isEmptyObject(obj) {
-      return Object.getOwnPropertyNames(obj).length == 0;
+      return Object.getOwnPropertyNames(obj).length === 0;
   }
 
   function indentConfig() {
@@ -66,7 +75,7 @@ export function activate(context: vscode.ExtensionContext) {
     // we do nothing if we have {} to not interrupt other extensions for indent settings
     if(! isEmptyObject(indentSetter) ) {
       var langCfg = indentSetter[ activeEditor.document.languageId ];
-      if( langCfg == undefined ) {
+      if( langCfg === undefined ) {
         // if we do not have any defaults get those from the editor config itself
         // this seems to break detectindentation = true :(
         langCfg = vscode.workspace.getConfiguration('editor');
@@ -74,26 +83,26 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.window.activeTextEditor.options = {
         "tabSize": langCfg.tabSize,
         "insertSpaces": langCfg.insertSpaces
-      }
+      };
     }
   }
 
   function checkLanguage() {
     if (activeEditor) {
-      if(currentLanguageId != activeEditor.document.languageId) {
+      if(currentLanguageId !== activeEditor.document.languageId) {
         var inclang = vscode.workspace.getConfiguration('indentRainbow')['includedLanguages'] || [];
         var exclang = vscode.workspace.getConfiguration('indentRainbow')['excludedLanguages'] || [];
 
         currentLanguageId = activeEditor.document.languageId;
         doIt = true;
-        if(inclang.length != 0) {
-          if(inclang.indexOf(currentLanguageId) == -1) {
+        if(inclang.length !== 0) {
+          if(inclang.indexOf(currentLanguageId) === -1) {
             doIt = false;
           }
         }
 
-        if(doIt && exclang.length != 0) {
-          if(exclang.indexOf(currentLanguageId) != -1) {
+        if(doIt && exclang.length !== 0) {
+          if(exclang.indexOf(currentLanguageId) !== -1) {
             doIt = false;
           }
         }
@@ -101,13 +110,11 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     if( clearMe && ! doIt) {
-      // clear decorations when language switches away
+      // Clear decorations when language switches away
       var decor: vscode.DecorationOptions[] = [];
-      activeEditor.setDecorations(decorationType[0], decor);
-      activeEditor.setDecorations(decorationType[1], decor);
-      activeEditor.setDecorations(decorationType[2], decor);
-      activeEditor.setDecorations(decorationType[3], decor);
-      activeEditor.setDecorations(decorationType[4], decor);
+      for (let decorationType of decorationTypes) {
+        activeEditor.setDecorations(decorationType, decor);  
+      }
       clearMe = false;
     }
 
@@ -131,51 +138,48 @@ export function activate(context: vscode.ExtensionContext) {
     }
     var regEx = /^[\t ]+/gm;
     var text = activeEditor.document.getText();
-    var tabsize = activeEditor.options.tabSize
-    var tabs = " ".repeat(tabsize)
-    var decor0: vscode.DecorationOptions[] = [];
-    var decor1: vscode.DecorationOptions[] = [];
-    var decor2: vscode.DecorationOptions[] = [];
-    var decor3: vscode.DecorationOptions[] = [];
-    var decor4: vscode.DecorationOptions[] = [];
+    var tabsize = activeEditor.options.tabSize;
+    var tabs = " ".repeat(tabsize);
+    let error_decorator: vscode.DecorationOptions[] = [];
+    let decorators = [];
+    decorationTypes.forEach(() => {
+      let decorator: vscode.DecorationOptions[] = [];
+      decorators.push(decorator);
+    });
+
     var re = new RegExp("\t","g");
     var match;
     while (match = regEx.exec(text)) {
       var ma = (match[0].replace(re, tabs)).length;
-      if(ma % tabsize != 0) {
+      if(ma % tabsize !== 0) {
         var startPos = activeEditor.document.positionAt(match.index);
         var endPos = activeEditor.document.positionAt(match.index + match[0].length);
         var decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: null };
-        decor4.push(decoration);
+        error_decorator.push(decoration);
       } else {
-        var m = match[0]
-        var l = m.length
-        var o = 0
-        var n = 0
+        var m = match[0];
+        var l = m.length;
+        var o = 0;
+        var n = 0;
         while(n < l) {
           var startPos = activeEditor.document.positionAt(match.index + n);
-          if(m[n] == "\t") {
+          if(m[n] === "\t") {
             n++;
           } else {
             n+=activeEditor.options.tabSize;
           }
           var endPos = activeEditor.document.positionAt(match.index + n);
           var decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: null };
-          switch(o % 4) {
-            case 0: decor0.push(decoration); break;
-            case 1: decor1.push(decoration); break;
-            case 2: decor2.push(decoration); break;
-            case 3: decor3.push(decoration); break;
-          }
+          let decorator_index = o % decorators.length;
+          decorators[decorator_index].push(decoration);
           o++;
         }
       }
     }
-    activeEditor.setDecorations(decorationType[0], decor0);
-    activeEditor.setDecorations(decorationType[1], decor1);
-    activeEditor.setDecorations(decorationType[2], decor2);
-    activeEditor.setDecorations(decorationType[3], decor3);
-    activeEditor.setDecorations(decorationType[4], decor4);
+    decorationTypes.forEach((decorationType, index) => {
+      activeEditor.setDecorations(decorationType, decorators[index]);
+    });
+    activeEditor.setDecorations(error_decoration_type, error_decorator);
     clearMe = true;
   }
 }
